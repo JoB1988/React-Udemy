0ea01ad4b8ba878891f79186a4ddb480e9d57ee5
Jonathan <jonathan_benevides@hotmail.com>
2020-03-16 17:26:25 -0300

    acrescentado um parâmetro no objeto

--- src/views/produtos/cadastro.model.js
+++ src/views/produtos/cadastro.model.js
@@ -1,9 +1,10 @@
 export class Produto {
-    constructor(name, sku, desc, price, provider) {
+    constructor(name, sku, desc, price, provider, id = undefined) {
         this.name = name;
         this.sku = sku;
         this.desc = desc;
         this.price = price;
         this.provider = provider;
+        this.id = id
     }
 }
