7a5315fccdc57146394620c15efb8ccddfc3815d
Jonathan <jonathan_benevides@hotmail.com>
2020-03-16 17:26:49 -0300

    novos serviços

--- src/views/produtos/produto.service.js
+++ src/views/produtos/produto.service.js
@@ -1,18 +1,44 @@
 import AXIOS from 'axios';
 
+let URL = `http://localhost:3000/produtos`;
+
 export default class CadastroService {
 
     salvar(produto) {
-        AXIOS.defaults.headers.common = {}
-        return AXIOS.post('http://localhost:3000/produtos', produto)
+        return AXIOS.post(`${URL}`, produto)
             .then(response => response.status)
             .catch(error => console.log(error))
             .finally()
     }
 
-    get() {
+    update(produto) {
+        return AXIOS.put(`${URL}/${produto.id}`, produto)
+            .then(response => response.status)
+            .catch(error => console.log(error))
+            .finally()
+    }
+
+    delete(id) {
+        return AXIOS.delete(`${URL}/${id}`)
+            .then(response => response.status)
+            .catch(error => console.log(error))
+            .finally()
+    }
+
+    getById(id) {
+        return AXIOS.get(`${URL}/${id}`)
+            .then(response => response.data)
+            .catch(error => console.log(error))
+            .finally()
+    }
+
+    getAll() {
+        var headers = [
+            { key: 'Content-Type', value: 'application/json' },
+            { key: 'Cache-Control', value: 'no-cache' }
+        ]
         AXIOS.defaults.headers.common = {}
-        return AXIOS.get('http://localhost:3000/produtos')
+        return AXIOS.get(URL, { headers })
             .then(response => response.data)
             .catch(error => console.log(error))
             .finally()
